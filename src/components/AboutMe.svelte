bbbbcf2d78367afabdaa858adf9db8122d40c785
David Freitag <freitagdavid@outlook.com>
2019-11-15 14:12:18 -0500

    Fixed broken link

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -1,39 +1,39 @@
 <style>
-    article {
-        margin: 58px auto;
-        width: 600px;
-    }
+  article {
+    margin: 58px auto;
+    width: 600px;
+  }
 
-    h3 {
-        margin-bottom: 10px;
-        font-weight: 700;
-        font-size: 1.5rem;
-        font-family: "open sans", sans-serif;
-    }
+  h3 {
+    margin-bottom: 10px;
+    font-weight: 700;
+    font-size: 1.5rem;
+    font-family: "open sans", sans-serif;
+  }
 
-    p {
-        margin: 3px 0;
-        font-size: 1.15rem;
-        letter-spacing: 0.01rem;
-        line-height: 1.75rem;
-    }
+  p {
+    margin: 3px 0;
+    font-size: 1.15rem;
+    letter-spacing: 0.01rem;
+    line-height: 1.75rem;
+  }
 </style>
 
 <article>
-    <h3>About me</h3>
-    <p>I'm David Freitag, welcome to my portfolio site,</p>
-    <p>
-        I'm a full stack developer with experience in javascript, react, node,
-        python, and a little bit of C. But don't let that fool you I'm always up
-        for learning a new language. To give a bit of background I was working a
-        job as a back office operator at a credit union data processor. I did
-        that for about a year then I decided that it wasn't for me. I have
-        always had an interest in programming but I never did anything with it
-        until I decided to go to Lambda. Doing this has been one of the best
-        decisions I have ever made.
-    </p>
-    <p>
-        If you would be interested in talking to me please feel free to
-        <a href="contact.html">contact me</a>
-    </p>
+  <h3>About me</h3>
+  <p>I'm David Freitag, welcome to my portfolio site,</p>
+  <p>
+    I'm a full stack developer with experience in javascript, react, node,
+    python, and a little bit of C. But don't let that fool you I'm always up for
+    learning a new language. To give a bit of background I was working a job as
+    a back office operator at a credit union data processor. I did that for
+    about a year then I decided that it wasn't for me. I have always had an
+    interest in programming but I never did anything with it until I decided to
+    go to Lambda. Doing this has been one of the best decisions I have ever
+    made.
+  </p>
+  <p>
+    If you would be interested in talking to me please feel free to
+    <a href="#/contact">contact me</a>
+  </p>
 </article>
